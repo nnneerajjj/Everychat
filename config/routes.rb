aa9c1fea998b5f30fe1d9b838962a60c6f36ce7b
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-05 22:40:34 +0900

    Add create user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get    'sign_in'      => 'users/sessions#new',     as: 'new_user_session'
-  post   'authenticate' => 'users/sessions#create',  as: 'create_user_session'
-  delete 'sign_out'     => 'users/sessions#destroy', as: 'destroy_user_session'
+  get    'users/sessions/new' => 'users/sessions#new',     as: 'new_user_session'
+  post   'users/sessions'     => 'users/sessions#create',  as: 'create_user_session'
+  delete 'users/sessions'     => 'users/sessions#destroy', as: 'destroy_user_session'
 
-  get    'users/new'         => 'users/registrations#new',     as: 'new_user_registration'
-  post   'users/create'      => 'users/registrations#create',  as: 'create_user_registration'
-  get    'users/:id/edit'    => 'users/registrations#edit',    as: 'edit_user_registration'
-  put    'users/:id/update'  => 'users/registrations#update',  as: 'update_user_registration'
-  delete 'users/:id/destroy' => 'users/registrations#destroy', as: 'destroy_user_registration'
+  resources :users, controller: 'users/registrations', only: %i(new edit create update destroy)
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
