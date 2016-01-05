2c206087c627f85fc493289205e2a5f7d5d8dfb4
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-06 00:07:18 +0900

    Implement user session utility module

---
+++ app/controllers/concerns/user_sessions.rb
@@ -0,0 +1,20 @@
+module UserSessions
+  extend ActiveSupport::Concern
+
+  def current_user
+    User.find(session[:user_id]) if user_signed_in?
+  end
+
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to new_user_session_path
+      # flash[:alert]
+    else
+      # flash[:notice]
+    end
+  end
+
+  def user_signed_in?
+    return session.key?(:user_id) && User.find(session[:user_id])
+  end
+end
