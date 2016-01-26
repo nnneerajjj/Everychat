669b75a79dc3ed7f8ebedfdd4afc84db30569f9e
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-26 16:57:57 +0900

    Generate rooms controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :users, controller: 'users/registrations', only: %i(new edit create update destroy)
 
+  resources :rooms, only: %i(index show create update destroy)
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
