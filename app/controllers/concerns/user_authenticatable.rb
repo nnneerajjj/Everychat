99b9dc883261441d6b1549f366df44b683bd0c6c
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-26 20:16:22 +0900

    Make user auth method apply to 'DRY' a kind of coding rule

--- app/controllers/concerns/user_authenticatable.rb
+++ app/controllers/concerns/user_authenticatable.rb
@@ -1,6 +1,10 @@
 module UserAuthenticatable
   extend ActiveSupport::Concern
 
+  included do
+    helper_method :current_user, :user_signed_in?
+  end
+
   def current_user
     User.find(session[:user_id]) if user_signed_in?
   end
--- app/helpers/user_helper.rb
+++
@@ -1,9 +0,0 @@
-module UserHelper
-  def current_user
-    User.find(session[:user_id]) if user_signed_in?
-  end
-
-  def user_signed_in?
-    session.key?(:user_id) && User.find(session[:user_id])
-  end
-end
