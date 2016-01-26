801b8c5079305de3866b005d45e0455147860c7b
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-26 17:17:43 +0900

    Add resource elements

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,16 +1,32 @@
 class RoomsController < ApplicationController
   def index
+    @room = Room.new
+    @rooms = Room.all
   end
 
   def show
+    @room = Room.find(params[:id])
   end
 
   def create
+    if Room.create(room_params)
+      flash[:notice] = 'Room was created successfully.'
+    else
+      flash[:alert] = 'Failed to create a room.'
+    end
+
+    redirect_to rooms_path
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
