a3c2d502f2470981838cf31debeaff9339aef2ff
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-26 23:32:37 +0900

    Implement Room#entry!(user)

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,9 @@
 class Room < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :messages
+
+  def entry!(user)
+    self.users << user
+    save!
+  end
 end
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -2,11 +2,13 @@ require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
   setup do
-    @room = Room.new(name: 'name')
+    @user = users(:user_1)
+    @room = Room.create(name: 'name')
   end
 
   test 'room should be valid' do
-    assert @room.validate
+    room = Room.new(name: 'name')
+    assert room.validate
   end
 
   test 'room has many users' do
@@ -15,5 +17,10 @@ class RoomTest < ActiveSupport::TestCase
 
   test 'room has many messages' do
     assert @room.respond_to?(:messages)
+  end
+
+  test '#entry! user' do
+    @room.entry!(@user)
+    assert @room.users.include?(@user)
   end
 end
