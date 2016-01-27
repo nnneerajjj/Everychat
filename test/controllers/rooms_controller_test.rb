719fcaa475053f407faa274672c196215ace5949
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-27 10:53:17 +0900

    Restrict guest user access to rooms

--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class RoomsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    user = User.create(name: 'name', password: 'pass', password_confirmation: 'pass')
+
+    post create_user_session_path,
+      params: { user: { name: 'name', password: 'pass' } }
+  end
+
   test 'should get rooms' do
     get rooms_path
     assert_response :success
