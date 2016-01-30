36894fdd5366670ce54bfd63b2beb76dbb44b60d
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-30 18:12:19 +0900

    Add user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :rooms
   has_many :messages
+
+  validates :name,
+    presence: true,
+    format: { with: /\A[0-9a-zA-Z\-_]+\z/ },
+    length: { maximum: 30 },
+    uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,11 +2,34 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   setup do
-    @user = User.new(name: 'user', password: 'pass', password_confirmation: 'pass')
+    @user = User.new(name: 'user', password: 'pass')
   end
 
   test 'validate successfully' do
     assert @user.validate, 'user should be valid'
+  end
+
+  test 'user name should not be blank' do
+    user = User.new(password: 'pass')
+    assert_not user.validate
+  end
+
+  test 'user name should be uniqueness' do
+    same_name = 'user_same_name'
+    user1 = User.new(name: same_name, password: 'pass')
+    user2 = User.new(name: same_name, password: 'pass')
+    user1.save
+    assert_not user2.save
+  end
+
+  test 'user name should be only alphabetic, numeric, hyphen and underscore' do
+    assert User.new(name: '-_', password: 'pass').validate
+    assert User.new(name: '1234', password: 'pass').validate
+    assert User.new(name: 'user_name-1234', password: 'pass').validate
+    assert_not User.new(name: 'user/1', password: 'pass').validate
+    assert_not User.new(name: 'user<1>', password: 'pass').validate
+    assert_not User.new(name: 'ユーザ名', password: 'pass').validate
+    assert_not User.new(name: 'abcdefghijklmnopqrstuvwxyz12345', password: 'pass').validate
   end
 
   test 'user has many messages' do
