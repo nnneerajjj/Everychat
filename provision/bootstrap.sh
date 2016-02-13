a782897e16eaf2d4ac68f0790b2303de28f785e2
Shota Iguchi <e.iguchi1124@gmail.com>
2016-02-14 02:59:17 +0900

    Simplify vagrant setup scripts

--- provision/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-apt-get update -y
-apt-get upgrade -y
-apt-get install -y git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev \
-  postgresql libpq-dev zlib1g-dev nodejs redis-server
-
-source /vagrant/provision/rbenv.sh
-
-cp /vagrant/provision/sudoers /etc/sudoers
--- provision/setup.sh
+++ provision/setup.sh
@@ -1,3 +1,8 @@
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev \
+  postgresql libpq-dev zlib1g-dev nodejs redis-server
+
 echo 'export RBENV_ROOT="/usr/local/rbenv"' | tee -a /etc/profile.d/rbenv.sh
 echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"'| tee -a /etc/profile.d/rbenv.sh
 echo 'eval "$(rbenv init -)"' | tee -a /etc/profile.d/rbenv.sh
@@ -11,3 +16,5 @@ rbenv global 2.2.4
 rbenv rehash
 
 chown -R vagrant ${RBENV_ROOT}
+
+cp /vagrant/provision/sudoers /etc/sudoers
