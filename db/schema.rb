7e8222ff5d9b8186f92054d4d3c7777cdb9b8d85
Neeraj Singh <nnneerajjj@gmail.com>
2016-04-17 12:49:54 +0530

    initial

--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,10 @@ ActiveRecord::Schema.define(version: 20160103083702) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["room_id"], name: "index_messages_on_room_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
+
+  add_index "messages", ["room_id"], name: "index_messages_on_room_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "rooms", force: :cascade do |t|
     t.string   "name"
@@ -32,9 +33,10 @@ ActiveRecord::Schema.define(version: 20160103083702) do
   create_table "rooms_users", force: :cascade do |t|
     t.integer "room_id"
     t.integer "user_id"
-    t.index ["room_id"], name: "index_rooms_users_on_room_id"
-    t.index ["user_id"], name: "index_rooms_users_on_user_id"
   end
+
+  add_index "rooms_users", ["room_id"], name: "index_rooms_users_on_room_id"
+  add_index "rooms_users", ["user_id"], name: "index_rooms_users_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
